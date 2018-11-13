fe9c4297594c460516b08e7e2c633d2a904a2343
HangTT <42291235+HangTT@users.noreply.github.com>
2018-11-13 19:56:41 +1000

    first upload

---
+++ script/menu.js
@@ -0,0 +1,35 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+function myMap() {
+    var mapProp= {
+        center:new google.maps.LatLng(51.508742,-0.120850),
+        zoom:5,
+    };
+    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
+
+    var slideIndex = 0;
+    showSlides();
+    
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+           slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
---
+++ script/test.js
@@ -0,0 +1,20 @@
+var x = 10;
+console.log(x);
+
+x += 10;
+console.log(x);
+
+if (x === 30) {
+    console.log("+= is the same!")
+}
+
+else {
+   console.log("is the else part...")
+}
+
+var myEl = document.getElementById('tom');
+myEl.innerHTML = "hello world";
+
+var y = 10;
+console.log("y="+10);
+
